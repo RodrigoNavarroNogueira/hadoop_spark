67c53deec0e022d34fc414c7260d6a2658dd54d1
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2024-09-05 03:36:39 -0300

    Atualizando script auctions.py

--- jobs/bronze/auctions.py
+++ jobs/bronze/auctions.py
@@ -1,60 +1,27 @@
-import requests
-import json
-from dotenv import load_dotenv
-import os
-import sys
-import time
 from logger.logger import logging
-import subprocess
+from jobs_functions.functions import *
 
-load_dotenv()
-token = os.getenv('API_TOKEN')
+current_file = 'auctions'
 
 url = 'https://us.api.blizzard.com/data/wow/connected-realm/3209/auctions?namespace=dynamic-us&locale=pt_BR'
 
-if not token:
-    logging.error('API token not found. Make sure its set in the .env')
-    exit()
+headers = verify_token()
 
-headers = {
-    'Authorization': f'Bearer {token}'
-}
-
-logging.info('Starting request, please wait')
+logging.info('Starting request, please wait...')
 
 try:
-    response = requests.get(url, headers=headers)
-    raw_data = response.json()
-    logging.info('Request response OK! Data was extracted')
-    data_json = json.dumps(raw_data)
+    data_bronze = request_data(url, headers)
     logging.info('All items in the Azralon server auction house have been collected')
 
 except requests.exceptions.JSONDecodeError:
     logging.error('API token is incorrect, please generate a new token and restart code-server')
     exit()
 
-time = list(time.localtime())
 
-file_name = f'auctions_{time[0]}-{time[1]}-{time[2]}_{time[3]}-{time[4]}-{time[5]}.json'
-
-container_name = "da-spark-yarn-master"
-
-hdfs_path = f"/opt/spark/data/wow/azralon/auctions/bronze/{file_name}"
-
-command = f"hdfs dfs -put - {hdfs_path}"
+hdfs_path = create_filename_hdfspath(current_file)
 
 try:
-    process = subprocess.Popen(
-        ["docker", "exec", "-i", container_name, "bash", "-c", command],
-        stdin=subprocess.PIPE,
-        text=True
-    )
-    process.communicate(input=data_json)
-
-    if process.returncode == 0:
-        logging.info('Data successfully uploaded to HDFS!')
-    else:
-        logging.error(f'Error sending data: Return code {process.returncode}')
+    insert_into_hdfs(hdfs_path, data_bronze)
 
 except subprocess.CalledProcessError as e:
     logging.error(f'Error executing command: {e}')
