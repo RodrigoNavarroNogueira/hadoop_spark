3bfb46ee3b8df210d13290e7c745c85000c1f04a
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2024-09-06 16:09:20 -0300

    Configurando o log path

--- logger/logger.py
+++ logger/logger.py
@@ -1,16 +1,23 @@
 import logging
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s %(levelname)s %(filename)s: %(message)s',
-    filename='/home/navarro/hadoop_spark/logs/auctions/bronze/auctions.log',
-    encoding='utf-8',
-    filemode='a',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
 
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-console_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(filename)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S"))
+def logger_conf(log_path: str) -> None:
+    """
+    Configures the logger according to the job that is executed
+    
+    param log_path: Path where the log file will be saved
+    """
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)s %(filename)s: %(message)s',
+        filename=log_path,
+        encoding='utf-8',
+        filemode='a',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
-logging.getLogger().addHandler(console_handler)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(filename)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S"))
+
+    logging.getLogger().addHandler(console_handler)
