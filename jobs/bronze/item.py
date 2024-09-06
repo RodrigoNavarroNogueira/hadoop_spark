a2049e8857bb3c0752e9ded0bc6d70cb89850a1f
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2024-09-06 16:08:11 -0300

    Atualizando arquivo

--- jobs/bronze/item.py
+++ jobs/bronze/item.py
@@ -1,60 +1,26 @@
-import requests
-import json
-from dotenv import load_dotenv
-import os
-import sys
-import time
-from logger.logger import logging
-import subprocess
+from logger.logger import logger_conf
+from jobs_functions.functions import *
 
-load_dotenv()
-token = os.getenv('API_TOKEN')
+current_file = 'item'
 
-url = 'https://us.api.blizzard.com/data/wow/item/120953?namespace=static-us&locale=pt_BR'
+id_item = 120953
 
-if not token:
-    logging.error('API token not found. Make sure its set in the .env')
-    exit()
+logger_conf('/home/navarro/hadoop_spark/logs/item.log')
 
-headers = {
-    'Authorization': f'Bearer {token}'
-}
+url = f'https://us.api.blizzard.com/data/wow/item/{id_item}?namespace=static-us&locale=pt_BR'
 
-logging.info('Starting request, please wait')
+logging.info('Starting request, please wait...')
 
 try:
-    response = requests.get(url, headers=headers)
-    raw_data = response.json()
-    logging.info('Request response OK! Data was extracted')
-    data_json = json.dumps(raw_data)
-    logging.info('All items in the Azralon server auction house have been collected')
+    data_bronze = request_data(url)
+    logging.info(f'All data for item ID {id_item} has been collected')
 
 except requests.exceptions.JSONDecodeError:
-    logging.error('API token is incorrect, please generate a new token and restart code-server')
+    logging.error('Could not convert response to json, please check API token')
     exit()
 
-time = list(time.localtime())
+hdfs_path = create_filename(current_file)
+logging.info('The file was created along with its path in HDFS')
 
-file_name = f'item_{time[0]}-{time[1]}-{time[2]}_{time[3]}-{time[4]}-{time[5]}.json'
-
-container_name = "da-spark-yarn-master"
-
-hdfs_path = f"/opt/spark/data/wow/azralon/item/bronze/{file_name}"
-
-command = f"hdfs dfs -put - {hdfs_path}"
-
-try:
-    process = subprocess.Popen(
-        ["docker", "exec", "-i", container_name, "bash", "-c", command],
-        stdin=subprocess.PIPE,
-        text=True
-    )
-    process.communicate(input=data_json)
-
-    if process.returncode == 0:
-        logging.info('Data successfully uploaded to HDFS!')
-    else:
-        logging.error(f'Error sending data: Return code {process.returncode}')
-
-except subprocess.CalledProcessError as e:
-    logging.error(f'Error executing command: {e}')
+insert_into_hdfs(hdfs_path, data_bronze)
+logging.info('File was successfully inserted into HDFS, ending job!')
