79e2ad794b4bab81ec6ddd725a4f141361067332
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2024-09-06 16:08:31 -0300

    Criando os annotations e doc strings

--- jobs_functions/functions.py
+++ jobs_functions/functions.py
@@ -3,11 +3,19 @@ import json
 from dotenv import load_dotenv
 import os
 import time
-from logger.auctions.logger import logging
+from logger.logger import logging
 import subprocess
 
 
-def request_data(url):
+def request_data(url: str) -> str:
+    """
+    Makes a GET request to the provided URL, using the token from the API_TOKEN
+    environment variable in the .env file. It also checks whether the variable
+    is a valid value and whether the response code
+    
+    param url: The URL to which the request will be made.
+    return: The bronze layer data in JSON format as a string.
+    """
     load_dotenv()
     token = os.getenv('API_TOKEN')
     headers = {'Authorization': f'Bearer {token}'}
@@ -28,14 +36,29 @@ def request_data(url):
         return data_bronze
 
 
-def create_filename(current_file):
+def create_filename(current_file: str) -> str:
+    """
+    Creates the name of the file that will be inserted into HDFS along with its path.
+    The file is created based on your endpoint and current date/time.
+
+    param current_file: The endpoint where we are extracting the data.
+    return: The directory where the file will be inserted into HDFS.
+    """
     current_time = list(time.localtime())
     file_name = f'{current_file}_{current_time[0]}-{current_time[1]}-{current_time[2]}_{current_time[3]}-{current_time[4]}-{current_time[5]}.json'
     hdfs_path = f"/opt/spark/data/wow/azralon/{current_file}/bronze/{file_name}"
     return hdfs_path
 
 
-def insert_into_hdfs(hdfs_path, data_bronze):
+def insert_into_hdfs(hdfs_path: str, data_bronze: str) -> None:
+    """
+    Interacts with the running container to execute the hdfs dfs -put command
+    inserting the bronze layer data to the specified HDFS path.
+    Also checks if the insertion was successful or if the container is not running.
+
+    param hdfs_path: The directory where the file will be inserted into the HDFS.
+    param data_bronze: The bronze layer data that will be inserted.
+    """
     container_name = "da-spark-yarn-master"
     command = f"hdfs dfs -put - {hdfs_path}"
     process = subprocess.Popen(
